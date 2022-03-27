cbb2404a7e5cf05044448979c03571e621580b19
jillexdxd <96015392+jillexdxd@users.noreply.github.com>
2022-03-27 20:40:27 +0200

    nueva parte

--- Proyecto final java/sudoku/src/sudoku/Juego.java
+++ Proyecto final java/sudoku/src/sudoku/Juego.java
@@ -1,16 +1,35 @@
 package sudoku;
 
-public class Juego {
-	public static void main(String[] args) {
-		Tablero tb = new Tablero();
+import java.util.Scanner;
 
-		System.out.println("░██████╗██╗░░░██╗██████╗░░█████╗░██╗░░██╗██╗░░░██");
-		System.out.println("██╔════╝██║░░░██║██╔══██╗██╔══██╗██║░██╔╝██║░░░██║");
-		System.out.println("╚█████╗░██║░░░██║██║░░██║██║░░██║█████═╝░██║░░░██║");
-		System.out.println("░╚═══██╗██║░░░██║██║░░██║██║░░██║██╔═██╗░██║░░░██║");
-		System.out.println("██████╔╝╚██████╔╝██████╔╝╚█████╔╝██║░╚██╗╚██████╔╝");
-		System.out.println("╚═════╝░░╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝░╚═════╝░");
-		System.out.println();
+public class Juego {
+	
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		Tablero tb = new Tablero();
+		Juego m = new Juego();
+		
 		tb.imprimir();
+		
+		int opcion;
+		do {
+			System.out.println("\n\t\t1. Rellenar celda");
+			System.out.println("\t\t2. Borrar celda");
+			System.out.println("\t\t3. Comprobar resultado");
+			System.out.print("\n\t\tIntroduce una opcion: ");
+			opcion = teclado.nextInt();
+			
+			
+		}	while(opcion <=0 && opcion > 3);
+		
+		switch (opcion){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+		}
 	}
 }
--- Proyecto final java/sudoku/src/sudoku/Tablero.java
+++ Proyecto final java/sudoku/src/sudoku/Tablero.java
@@ -1,14 +1,126 @@
 package sudoku;
 
+
 public class Tablero {
-	private int[][] tab;
-	
+	private int[][] tab, masc;
+
 	public Tablero() {
+		System.out.println("░██████╗██╗░░░██╗██████╗░░█████╗░██╗░░██╗██╗░░░██╗");
+		System.out.println("██╔════╝██║░░░██║██╔══██╗██╔══██╗██║░██╔╝██║░░░██║");
+		System.out.println("╚█████╗░██║░░░██║██║░░██║██║░░██║█████═╝░██║░░░██║");
+		System.out.println("░╚═══██╗██║░░░██║██║░░██║██║░░██║██╔═██╗░██║░░░██║");
+		System.out.println("██████╔╝╚██████╔╝██████╔╝╚█████╔╝██║░╚██╗╚██████╔╝");
+		System.out.println("╚═════╝░░╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝░╚═════╝░");
+		System.out.println();
+		
+		// relleno la matriz por cada fila elementos del 1 al 4 que toma el valor c+1 para no tener que controlar que se repitan en la misma fila
 		tab = new int[4][4];
 		for (int f = 0; f < tab.length; f++) {
 			for (int c = 0; c < tab[f].length; c++){
-				tab[f][c] = 0;
+				tab[f][c] = c+1;
 			}
+		}
+		
+		boolean control = true;
+		
+		do {
+			// Algoritmo de sattolo funciona en indice negativo para no repetir posiciones e ir descartando elementos de la lista al permutarlos
+			for (int f = 0; f < tab.length; f++) {
+				for (int i = 4; i > 1; i--) {
+					int r = (int) (Math.random() * (i-1));
+					int aux = tab[f][r];
+					tab[f][r] = tab[f][i-1];
+					tab[f][i-1] = aux;
+				}
+			}
+			
+			// Inicia un for para llevar el control de numeros repetidos en la misma fila despues de la permutacion del vector
+			// Al ser permutado con unos valores del 1-4 en el vector, es imposible que se repitan en f, pero si es posible que se repitan en c
+			int con1 = 0, con2 = 0, con3 = 0, con4 = 0, contf = 0;
+					
+			for (int c = 0; c < 4; c++){
+				for (int f = 0; f < 4; f++) {
+					if (tab[f][c] == 1) {
+						con1++;
+					}
+					else if (tab[f][c] == 2) {
+						con2++;
+					}
+					else if (tab[f][c] == 3) {
+						con3++;
+					}
+					else if (tab[f][c] == 4) {
+						con4++;
+					}
+				}
+				//Se comprueba que hay uno por cada fila y se suma a contf 1, para un estado correcto
+				// contf, despues de este bucle tiene que ser = 4
+				if (con1 == 1 && con2 == 1 && con3 == 1 && con4 == 1) {
+					contf++;
+				}
+				
+				con1 = 0;
+				con2 = 0;
+				con3 = 0;
+				con4 = 0;
+			}
+			
+			// Se comprueba que haya al menos un numero de cada en la primer 2x2 empezando desde arriba a la izquierda
+			// Casualmente coincide en los resultados que los demas bloques 2x2 son un numero en cada celda
+
+			for (int f = 0; f < tab.length/2; f++) {
+				for (int c = 0; c < tab[f].length/2; c++) {
+					if (tab[f][c] == 1) {
+						con1++;
+					}
+					else if (tab[f][c] == 2) {
+						con2++;
+					}
+					else if (tab[f][c] == 3) {
+						con3++;
+					}
+					else if (tab[f][c] == 4) {
+						con4++;
+					}
+				}
+			}
+			
+			// si se comprueba un numero en cada celda del primer 2x2 contf suma 1
+			// siendo la salida 5, el dato que le tenemos que pasar abajo linea(88)
+			
+			if (con1 == 1 && con2 == 1 && con3 == 1 && con4 == 1) {
+				contf++;
+			}
+			
+			//si es 5, se convierte en false y sale del bucle do while dando por terminada la mezcla de los numeros
+			if (contf == 5) {
+				control = false;
+			} else {
+				control = true;
+			}
+			
+		} while (control);
+		
+		
+		//llama al metodo que marca los numeros que se ven
+		pista();
+	}
+	
+	public void pista() {
+		masc = new int[4][4];
+		
+		for (int f = 0; f < 4; f++) {
+			for (int c = 0; c < 4; c++){
+				masc[f][c] = 0;
+			}
+		}
+		
+		int randF, randC;
+		
+		for (int i = 0; i <= 8; i++) {
+			randF = (int) Math.floor(Math.random()*4);
+			randC = (int) Math.floor(Math.random()*4);
+			masc[randF][randC] = 1;
 		}
 	}
 	
@@ -17,7 +129,11 @@ public class Tablero {
 		for (int f = 0; f < tab.length; f++) {
 			System.out.print("\t\t| ");
 			for (int c = 0; c < tab[f].length; c++){
-				System.out.print(tab[f][c] + " | ");
+				if (masc[f][c] == 1) {
+					System.out.print(tab[f][c] + " | ");
+				}	else {
+					System.out.print(" " + " | ");
+				}
 			}
 			System.out.println("\n\t\t+---+---+---+---+");
 		}
